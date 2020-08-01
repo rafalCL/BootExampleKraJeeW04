c86dec3cfe3ff7df5284373833399baac703c517
Rafal <rafal.opial@coderslab.pl>
2020-08-01 15:18:03 +0200

    Lombok example

--- src/main/java/pl/coderslab/bootexamplekrajeew04/controller/HomeController.java
+++ src/main/java/pl/coderslab/bootexamplekrajeew04/controller/HomeController.java
@@ -2,11 +2,17 @@ package pl.coderslab.bootexamplekrajeew04.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.bootexamplekrajeew04.entity.Person;
 
 @Controller
 public class HomeController {
     @GetMapping
     public String home(){
+        Person person = Person.builder()
+                .firstName("imie")
+                .lastName("nazwisko")
+                .build();
+
         return "home";
     }
 }
---
+++ src/main/java/pl/coderslab/bootexamplekrajeew04/entity/Person.java
@@ -0,0 +1,23 @@
+package pl.coderslab.bootexamplekrajeew04.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@Builder
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
